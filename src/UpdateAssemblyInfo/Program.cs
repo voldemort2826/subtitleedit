443eccd9b922c78f4e86be9bc365b7f5593e7097
voldemort2826 <102014910+voldemort2826@users.noreply.github.com>
2025-08-19 18:39:07 +0700

    address CA1305

--- src/UpdateAssemblyInfo/Program.cs
+++ src/UpdateAssemblyInfo/Program.cs
@@ -457,7 +457,7 @@ namespace UpdateAssemblyInfo
             var installer = Path.Combine(dir, "Nikse.SubtitleEdit.installer.yaml");
             var yaml = File.ReadAllText(installer);
             yaml = SetVariable(yaml, "PackageVersion", version.NormalVersion); //PackageVersion: 3.6.7.0
-            yaml = SetVariable(yaml, "ReleaseDate", DateTime.Now.ToString("yyyy-MM-dd")); //ReleaseDate: 2022-08-13
+            yaml = SetVariable(yaml, "ReleaseDate", DateTime.Now.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)); //ReleaseDate: 2022-08-13
             yaml = SetVariable(yaml, "InstallerUrl", $"https://github.com/SubtitleEdit/subtitleedit/releases/download/{version.ShortVersion}/SubtitleEdit-{version.ShortVersion}-Setup.exe"); //InstallerUrl: https://github.com/SubtitleEdit/subtitleedit/releases/download/3.6.7/SubtitleEdit-3.6.7-Setup.exe
             yaml = SetVariable(yaml, "InstallerSha256", sha256Hash); //InstallerSha256: 66F2BEFD07E2295EE606BC02A4EAACB1E0D2DEBE42B4D167AE45C5CC76F5E9A3
             File.WriteAllText(installer, yaml.TrimEnd()+ Environment.NewLine + Environment.NewLine);
@@ -494,7 +494,7 @@ namespace UpdateAssemblyInfo
                 var builder = new StringBuilder();
                 for (var i = 0; i < bytes.Length; i++)
                 {
-                    builder.Append(bytes[i].ToString("X2"));
+                    builder.Append(bytes[i].ToString("X2", CultureInfo.InvariantCulture));
                 }
 
                 return builder.ToString();
@@ -511,7 +511,7 @@ namespace UpdateAssemblyInfo
                 if (s.TrimStart().StartsWith(searchStr.TrimStart(), StringComparison.OrdinalIgnoreCase))
                 {
                     var start = line.Substring(0, line.IndexOf(searchStr, StringComparison.OrdinalIgnoreCase) + searchStr.Length);
-                    sb.AppendLine($"{start} {targetValue}");
+                    sb.AppendLine(CultureInfo.InvariantCulture, $"{start} {targetValue}");
                 }
                 else
                 {
